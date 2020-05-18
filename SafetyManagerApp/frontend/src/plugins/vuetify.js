2aeb5fb308a54e8e359f4cad2cebf21886e3b16c
Andre <andrecwj.18@gmail.com>
2020-05-19 01:28:12 +0800

    Added for Table GUI

--- SafetyManagerApp/frontend/src/plugins/vuetify.js
+++ SafetyManagerApp/frontend/src/plugins/vuetify.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 import light from './vuetify/lightTheme';
+import VueTableDynamic from 'vue-table-dynamic'
 
 Vue.use(Vuetify);
+Vue.use(VueTableDynamic);
 
 export default new Vuetify({
     theme: {
--- SafetyManagerApp/frontend/src/views/ManageLogs.vue
+++ SafetyManagerApp/frontend/src/views/ManageLogs.vue
@@ -3,11 +3,52 @@
         <v-layout>
             <h1>Manage Logs Page</h1>
         </v-layout>
+
+        <div id="example-1">
+            <button>Delete Row</button>
+        </div>
+
+        <div class="base-demo" style="width: 600px;">
+            <vue-table-dynamic :params="params"
+                               @select="onSelect"
+                               @selection-change="onSelectionChange"
+                               ref="table">
+            </vue-table-dynamic>
+        </div>
+
     </v-container>
 </template>
 <script>
-export default {
-    name: 'ManageLogs'
-};
+
+    import VueTableDynamic from 'vue-table-dynamic'
+    export default {
+        name: 'Demo',
+        data() {
+            return {
+                params: {
+                    data: [
+                        ['Timestamp', 'Missing PPE', 'Image of Incident'],
+                        [1, 'b3ba90', '7c95f7'],
+                        [2, 'ec0b78', 'ba045d'],
+                        [3, '63788d', 'a8c325']
+                    ],
+                    header: 'row',
+                    border:true,
+                    showCheck: true
+
+                }
+            }
+        },
+        methods: {
+            onSelect(isChecked, index, data) {
+                console.log('onSelect: ', isChecked, index, data)
+                console.log('Checked Data:', this.$refs.table.getCheckedRowDatas(true))
+            },
+            onSelectionChange(checkedDatas, checkedIndexs, checkedNum) {
+                console.log('onSelectionChange: ', checkedDatas, checkedIndexs, checkedNum)
+            }
+        },
+        components: { VueTableDynamic }
+    }
 </script>
 <style scoped></style>
