15ac237a6c9b364d1088562074fffe7eac7eaa99
Andre <andrecwj.18@gmail.com>
2020-05-22 14:47:47 +0800

    Table semi-working.

--- SafetyManagerApp/frontend/src/views/ManageLogs.vue
+++ SafetyManagerApp/frontend/src/views/ManageLogs.vue
@@ -1,4 +1,4 @@
-<template>
+<div>
     <v-container fluid>
         <div id="example-1">
             <button>Delete Row</button>
@@ -6,17 +6,22 @@
         <v-card>
             <v-data-table v-model="selected"
                           :headers="headers"
-                          :items="items"
+                          :items="itemList"
                           item-key="name"
                           show-select
                           class="elevation-1"
                           loading>
-                <template v-slot:top>
+                <template slot="items" slot-scope="props">
+                    <td>{{ props.item.id }}</td>
+                    <td class="text-xs-right">{{ props.item.isviolated }}</td>
+                    <td class="text-xs-right">{{ props.item.timestamp }}</td>
+                    <td class="text-xs-right">{{ props.item.equipment }}</td>
                 </template>
             </v-data-table>
         </v-card>
     </v-container>
-</template>
+</div>
+
 <script>
 
     import Axios from "axios"
@@ -29,15 +34,14 @@
                 itemList: [],
                 headers: [
                     { text: 'ID', value: 'id' },
-                    { text: 'Violation', value: 'violation' },
+                    { text: 'Violation', value: 'isviolated' },
                     { text: 'Timestamp', value: 'timestamp' },
-                    { text: 'Missing PPE', value: 'ppe' },
-                    { text: 'Image', value: 'image' }
-                ],
-                items: [
-                    Axios.get('http://localhost:65006/api/logs/')
+                    { text: 'Missing PPE', value: 'equipment' },
                 ]
             }
+        },
+        created() {
+            Axios.get('http://localhost:50550/api/logs/').then(response => (this.itemList = response.data.Success))
         }
     }
 </script>
